a9bbd2870eedfda7e5f433d300e684bf078bcecd
gitfav <wangrjmailbox@126.com>
2018-10-23 14:57:15 +0800

    修改文件名称

--- 10 PHP数据结构/课堂练习/maopao.php
+++ 10 PHP数据结构/课堂练习/maopao.php
@@ -1,18 +1,55 @@
 <?php
+/**
+ * Created by PhpStorm.
+ * User: jerise
+ * Date: 18-10-22
+ * Time: 上午10:08
+ */
 
-$arr = [10, 23, 32, 4, 6, 27, 38, 19];
+$arr = [69, 29, 33, 21, 2, 34, 332, 22];
 
-$num = count($arr) - 1;
 
-for ($i = 0; $i < $num; $i++) {
-    for ($j = 0; $j < $num; $j++) {
-        if ($arr[$j] > $arr[$j + 1]) {
-            $ex = $arr[$j];
-            $arr[$j] = $arr[$j + 1];
-            $arr[$j + 1] = $ex;
+/*
+|-----------------------------------------
+| 冒泡排序函数
+|-----------------------------------------
+*/
+function bubbleSort($arr)
+{
+//    $num = count($arr);
+//    $select = 0;
+//    while ($select < $num) {
+//        for ($i = $select + 1; $i < $num; $i++) {
+//            if ($arr[$select] > $arr[$i]) {
+//                $arr[$i] ^= $arr[$select];
+//                $arr[$select] ^= $arr[$i];
+//                $arr[$i] ^= $arr[$select];
+//            }
+//            $select = $i;
+//        }
+//        $num--;
+//        $select = 0;
+//    }
+//
+//    return $arr;
+
+
+    /*
+    |-----------------------------------------
+    |换一种写法
+    |-----------------------------------------
+    */
+    $len = count($arr);
+    for ($i = 0; $i < $len - 1; $i++) {
+        for ($j = 0; $j < $len - $i - 1; $j++) {
+            if ($arr[$j] > $arr[$j + 1]) {
+                $arr[$j] ^= $arr[$j + 1];
+                $arr[$j + 1] ^= $arr[$j];
+                $arr[$j] ^= $arr[$j + 1];
+            }
         }
     }
-    $num--;
+    return $arr;
 }
 
-var_dump($arr);
+var_dump(bubbleSort($arr));
